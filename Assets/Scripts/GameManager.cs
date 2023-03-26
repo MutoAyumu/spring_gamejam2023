9023b8c7be8bd75293659048acd65ae09ae7d52c
Akira Watanabe <a0323w@gmail.com>
2023-03-26 11:11:00 +0900

    [add] GameManagerの基礎部分の作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Tooltip("遷移先のシーン")]
+    [SerializeField] private SceneNames _sceneName = SceneNames.TITLE_SCENE;
+    [SerializeField] private Image _scorePanel = default;
+    [Header("テスト")]
+    [SerializeField] private Text _timerText = default;
+
+    private int _score = 0;
+    private float _timer = 0f;
+    private readonly Dictionary<SceneNames, string> _scenes = new()
+    {
+        [SceneNames.TITLE_SCENE] = "Title",
+        [SceneNames.GAME_SCENE] = "MainScene",
+        [SceneNames.RESULT_SCENE] = "Result",
+    };
+
+    private int _enemyID = 0;
+    private int _answerID = 9999;
+
+    public int AnswerID { get => _answerID; protected set => _answerID = value; }
+
+    private void Start()
+    {
+        _scorePanel.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+        _timerText.text = _timer.ToString("F0");
+
+        if (_timer <= 0f)
+        {
+            //TODO：スコア表示のパネル表示
+            _scorePanel.gameObject.SetActive(true);
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        _score = 0;
+        //TitleSceneに戻る
+        //SceneManager.LoadScene(_scenes[SceneNames.TITLE_SCENE]);
+        Debug.Log("GameOver");
+    }
+
+    public void GameClear()
+    {
+        //スコアの増加
+        _score = (int)(_timer * 100);
+        //シーン遷移(修正あるかも)
+        SceneManager.LoadScene(_scenes[_sceneName]);
+    }
+
+    public void SetTimer(float value)
+    {
+        _timer = value;
+    }
+
+    /// <summary> 遷移先のシーンの変更 </summary>
+    public void SceneNameUpdate()
+    {
+        if (SceneManager.GetActiveScene().name ==
+            _scenes[SceneNames.TITLE_SCENE])
+        {
+            _sceneName = SceneNames.GAME_SCENE;
+        }
+        else if (SceneManager.GetActiveScene().name ==
+            _scenes[SceneNames.GAME_SCENE])
+        {
+            _sceneName = SceneNames.RESULT_SCENE;
+        }
+        else if (SceneManager.GetActiveScene().name ==
+            _scenes[SceneNames.RESULT_SCENE])
+        {
+            _sceneName = SceneNames.TITLE_SCENE;
+        }
+
+    }
+
+    public void IdUpdate()
+    {
+
+    }
+}
+
+public enum SceneNames
+{
+    TITLE_SCENE,
+    GAME_SCENE,
+    RESULT_SCENE,
+}
---
+++ Assets/Scripts/GameManagerAttachment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManagerAttachment : MonoBehaviour
+{
+    [Tooltip("制限時間")]
+    [SerializeField] private float _timeLimit = 60f;
+
+    private GameManager _manager = default;
+
+    private void Awake()
+    {
+        _manager = GetComponent<GameManager>();
+
+        _manager.SetTimer(_timeLimit);
+        _manager.SceneNameUpdate();
+        _manager.IdUpdate();
+
+        Debug.Log("実行済");
+    }
+}
